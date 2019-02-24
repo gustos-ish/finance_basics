008c20dd435bd27d71d0f34f334d34af486143e5
aishema <ishema.auguste@gmail.com>
2019-02-24 11:20:08 +0100

    package creation

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='finance_basics',
+      version='0.1',
+      description='Educational projects for finance and python',
+      packages=find_packages(),
+      )
